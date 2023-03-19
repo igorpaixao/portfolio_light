f5f3f47f6a6a0afbdaae9dba44badf65178b53ec
Igor da Paixão <igorsantos.contato@outlook.com>
2023-03-19 08:59:25 -0300

    Projeto portfolio-light concluído

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function exibirNavegacao () {
+    
+   if (document.getElementById('nav-principal-mobile').style.display == 'none') {
+        document.getElementById('nav-principal-mobile').style.display = 'block';
+   }
+
+   else {
+        document.getElementById('nav-principal-mobile').style.display = 'none';
+   }
+}
